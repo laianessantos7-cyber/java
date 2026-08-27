8109965357cd577f731ad557bb4c6384a0b49964
Laiane <laianee12santos@gmail.com>
2026-08-27 14:49:22 -0300

    Add files via upload

---
+++ aulaarray/VetArray06.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class VetArray06 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes;
+        int[] idades;
+
+        System.out.println("Quantas pessoas deseja cadastrar?");
+        int quantidade = sc.nextInt();
+        sc.nextLine();
+
+        nomes = new String[quantidade];
+        idades = new int[quantidade];
+
+        for (int i = 0; i < quantidade; i++) {
+
+            System.out.println("Digite o nome:");
+            nomes[i] = sc.nextLine();
+
+            System.out.println("Digite a idade:");
+            idades[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.println(nomes[i] + " - " + idades[i] + " anos");
+        }
+
+        sc.close();
+    }
+}
